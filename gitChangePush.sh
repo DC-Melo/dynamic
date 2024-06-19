eeaa6c7179d73d813dbaab9fed950c7ea7552a1c
DC-Melo <melodachor@gmail.com>
2024-06-19 17:16:34 +0800

    Wed Jun 19 17:16:34 CST 2024

--- gitChangePush.sh
+++ gitChangePush.sh
@@ -2,7 +2,12 @@
 # cat $HOME/.config/.natapp.log | grep -oP 'server.natappfree.cc:\d+' | tail -1 >$HOME/.config/natapp.ip
 # # echo check is commited
 
-cd "${0:A:h}"
+# cd "${0:A:h}"
+# echo "${0:A:h}"
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+
 if [[ ! -z "$(git diff-index --name-only HEAD)" ]]; then
 	git add -u
 	git commit -m "$(date)"
