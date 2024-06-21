996a19fe06008a0027837c49a1d73c54fc035dc9
DC-Melo <melodachor@gmail.com>
2024-06-21 11:23:26 +0800

    2024年 06月 21日 星期五 11:23:26 CST

--- gitChangePush.sh
+++ gitChangePush.sh
@@ -1,12 +1,13 @@
+#!/bin/sh
 # logFile=$HOME/.cache/log/natapp.log
 # cat $HOME/.config/.natapp.log | grep -oP 'server.natappfree.cc:\d+' | tail -1 >$HOME/.config/natapp.ip
 # # echo check is commited
 
-# cd "${0:A:h}"
-# echo "${0:A:h}"
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-cd $SCRIPT_DIR
-echo $SCRIPT_DIR
+cd "${0:A:h}"
+echo "${0:A:h}"
+# SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+# cd $SCRIPT_DIR
+# echo $SCRIPT_DIR
 
 
 if [[ ! -z "$(git diff-index --name-only HEAD)" ]]; then
